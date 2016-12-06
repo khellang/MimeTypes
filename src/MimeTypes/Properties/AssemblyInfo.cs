791e8be65513adc4ba676a58c9cf9dd9d48ce9dd
Kristian Hellang <kristian@hellang.com>
2016-12-06 21:19:23 +0100

    Add contentFiles

--- src/MimeTypes/Properties/AssemblyInfo.cs
+++ src/MimeTypes/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("Generated lookup from file extension to mime type.")]
 [assembly: AssemblyCompany("Kristian Hellang")]
 [assembly: AssemblyProduct("MimeTypes")]
-[assembly: AssemblyCopyright("Copyright © Kristian Hellang 2015")]
+[assembly: AssemblyCopyright("Copyright © Kristian Hellang 2016")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("483a3b9c-59fe-4fba-9b2c-138dc5764d67")]
 
-[assembly: AssemblyVersion("1.0.5.0")]
-[assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyInformationalVersion("1.0.5")]
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyInformationalVersion("1.0.6")]
