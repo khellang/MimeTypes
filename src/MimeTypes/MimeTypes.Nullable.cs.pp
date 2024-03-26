4f213e310da13ac572cc1dc7b617748ec40042eb
Kristian Hellang <kristian@hellang.com>
2024-03-26 16:07:34 +0100

    Remove nullability for older targets. Fixes #22

--- src/MimeTypes/MimeTypes.Nullable.cs.pp
+++ src/MimeTypes/MimeTypes.Nullable.cs.pp
@@ -1283,25 +1283,4 @@ namespace $rootnamespace$
     }
 }
 
-namespace System.Diagnostics.CodeAnalysis
-{
-    /// <summary>Specifies that null is allowed as an input even if the corresponding type disallows it.</summary>
-    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property)]
-    internal sealed class AllowNullAttribute : Attribute { }
-
-    /// <summary>Specifies that when a method returns <see cref="ReturnValue"/>, the parameter will not be null even if the corresponding type allows it.</summary>
-    [AttributeUsage(AttributeTargets.Parameter)]
-    internal sealed class NotNullWhenAttribute : Attribute
-    {
-        /// <summary>Initializes the attribute with the specified return value condition.</summary>
-        /// <param name="returnValue">
-        /// The return value condition. If the method returns this value, the associated parameter will not be null.
-        /// </param>
-        public NotNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
-
-        /// <summary>Gets the return value condition.</summary>
-        public bool ReturnValue { get; }
-    }
-}
-
 #pragma warning enable
